58b8593d0fc65b653607cf22ce7d15385fcf48fc
Happyhp <hiyhp@outlook.com>
2024-02-27 23:11:48 +0800

    添加项目文件。

---
+++ 2.1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "Ð¡Ã÷£¬µØÇò" << endl;
+    return 0;
+
+}
---
+++ 2.2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout << "please input the distants in units of long" << endl;
+    int distance;
+    cin >> distance;
+    cout << 220 * distance << endl;
+    return 0;
+}
